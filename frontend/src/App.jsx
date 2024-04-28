168a3e8c977c45135c074c72d33fbc5778d7ed19
AhmadAamir <ahmadaamir781@gmail.com>
2024-04-29 01:13:10 +0500

    add Readme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,27 +27,3 @@ function App() {
 
 export default App;
 
-
-// import React from "react";
-// import "./App.css";
-// import Navbar from "./components/ui/Navbar";
-// import Container from "./components/ui/Container";
-// import HomePage from "./pages/HomePage";
-// import SignupPage from "./pages/SignupPage";
-// import LoginPage from "./pages/LoginPage";
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <Container>
-//         {/* Render components based on the route */}
-//         {window.location.pathname === "/" && <HomePage />}
-//         {window.location.pathname === "/signup" && <SignupPage />}
-//         {window.location.pathname === "/login" && <LoginPage />}
-//       </Container>
-//     </>
-//   );
-// }
-
-// export default App;
